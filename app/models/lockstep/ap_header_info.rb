f9b0c15a29dc76b3fb623095e828d4eed0320537
Sumit Sourav <sumit.sourav@lockstep.io>
2022-08-25 12:28:06 +0530

    removing ap/ar header changes

--- app/models/lockstep/ap_header_info.rb
+++
@@ -1,8 +0,0 @@
-class Lockstep::ApHeaderInfo < Lockstep::ApiRecord
-  self.model_name_uri = "v1/Reports/ap-header"
-  self.id_ref = "group_key"
-  self.query_path=""
-  load_schema(Schema::ApHeaderInfo)
-  single_record!
-
-end
