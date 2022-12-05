5756c5daac284279a4dab78cf35ca9a1c3714b01
Lockstep APAC <lockstepapac@INXRFG6YCJ6Y.local>
2022-12-05 11:27:55 +0530

    Minor change

--- spec/models/lockstep/feature_flag_spec.rb
+++ spec/models/lockstep/feature_flag_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Lockstep::FeatureFlag' do
 
-  context 'when generating' do
-    it 'should return 401 since api key not set' do
+  context 'when calling feature flag' do
+    it 'should return 200' do
       VCR.use_cassette("models/lockstep/feature_flag/exist") do
         response =  Lockstep::FeatureFlag.create({ names: ["Inbox.GlobalSearch"] })
         expect(response.code).eql?(200)
@@ -11,7 +11,7 @@ RSpec.describe 'Lockstep::FeatureFlag' do
       end
     end
 
-    it 'should return 404 since internal key not set' do
+    it 'should return false for the feature flag name' do
       VCR.use_cassette("models/lockstep/feature_flag/does_not_exist") do
         response =  Lockstep::FeatureFlag.create({ names: ["Inbox.GlobalSearch1"] })
         expect(response.attributes.values).eql?(false)
