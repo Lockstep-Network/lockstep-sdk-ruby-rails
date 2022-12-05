ee470e5ffadd9bca06989a42a4fefa0182db3053
Lockstep APAC <lockstepapac@INXRFG6YCJ6Y.local>
2022-12-05 14:43:59 +0530

    minor change

--- spec/models/lockstep/feature_flag_spec.rb
+++ spec/models/lockstep/feature_flag_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe 'Lockstep::FeatureFlag' do
 
   context 'when calling feature flag' do
     it 'should return true for the feature flag name' do
-      Lockstep::Client.set_api_key("LSPK-eRN6CJzSqdqakh0kDemOu2lMi0mWa0yg2PrdJoK5h2k5p22D/47fQJi/QdyXfY60dX7L5asbHfTIWXXVPVAvug==")
       VCR.use_cassette("models/lockstep/feature_flag/exist") do
         response = Lockstep::FeatureFlag.create({ names: ["Inbox.GlobalSearch1"] })
         expect(response.attributes.values).eql?(true)
@@ -12,7 +11,6 @@ RSpec.describe 'Lockstep::FeatureFlag' do
     end
 
     it 'should return false for the feature flag name' do
-      Lockstep::Client.set_api_key("LSPK-eRN6CJzSqdqakh0kDemOu2lMi0mWa0yg2PrdJoK5h2k5p22D/47fQJi/QdyXfY60dX7L5asbHfTIWXXVPVAvug==")
       VCR.use_cassette("models/lockstep/feature_flag/does_not_exist") do
         response = Lockstep::FeatureFlag.create({ names: ["Inbox.GlobalSearch1"] })
         expect(response.attributes.values).eql?(false)
