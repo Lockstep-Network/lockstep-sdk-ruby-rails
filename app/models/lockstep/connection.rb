f8c948735af9900c646d826977345d72a0d66ea1
Tejas Shetty <74608924+shettytejas@users.noreply.github.com>
2022-12-13 18:15:10 +0530

    Feature: Add 'Company' as type in Lockstep::Connection (#26)
    * Initial Commit
    
    * Initial Commit and Version Update
    
    * fix version
    
    * bundle install and updating specs

--- app/models/lockstep/connection.rb
+++ app/models/lockstep/connection.rb
@@ -1,19 +1,20 @@
 class Lockstep::Connection < Lockstep::ApiRecord
-  self.model_name_uri = "v1/Companies"
-  self.id_ref = "company_id"
+  self.model_name_uri = 'v1/Companies'
+  self.id_ref = 'company_id'
   load_schema(Schema::Company)
 
-  enum company_type: %w(Customer Vendor Group)
+  enum company_type: %w[Customer Vendor Group Company]
 
-  has_many :contacts, class_name: "Lockstep::Contact", included: true
-  belongs_to :created_user, class_name: "Lockstep::User", foreign_key: :created_user_id, primary_key: :user_id
-  belongs_to :modified_user, class_name: "Lockstep::User", foreign_key: :modified_user_id, primary_key: :user_id
+  has_many :contacts, class_name: 'Lockstep::Contact', included: true
+  belongs_to :created_user, class_name: 'Lockstep::User', foreign_key: :created_user_id, primary_key: :user_id
+  belongs_to :modified_user, class_name: 'Lockstep::User', foreign_key: :modified_user_id, primary_key: :user_id
 
   validates :company_name, presence: true
 
-  default_scope { where(company_type: %w(Customer Vendor Group)).or(where(company_type: nil)) }
+  default_scope { where(company_type: %w[Customer Vendor Group Company]).or(where(company_type: nil)) }
 
-  scope :customers, -> { where(company_type: "Customer") }
-  scope :vendors, -> { where(company_type: "Vendor") }
+  scope :customers, -> { where(company_type: 'Customer') }
+  scope :vendors, -> { where(company_type: 'Vendor') }
+  scope :internal_connections, -> { where(company_type: 'Company') }
   scope :company_type_null, -> { where(company_type: nil) }
 end
