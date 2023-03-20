936a932b284db907619326d7fa02281b3639bf72
Ashish <ashish@lockstep.io>
2023-03-20 18:06:55 +0530

    Added description for function

--- app/models/lockstep/service_fabric_payment.rb
+++ app/models/lockstep/service_fabric_payment.rb
@@ -4,10 +4,18 @@ class Lockstep::ServiceFabricPayment < Lockstep::ApiRecord
   self.query_path=""
   load_schema(Schema::SfPaymentRequest)
   
+  # Returning alwas false becase if id_ref is present in
+  # request body it returns true but in this model id_ref
+  # is required field in request body
+  # TODO: Make it generic 
   def persisted?
     false
   end
 
+  # Preventing deletion of id_ref from puts_attrs because
+  # id_ref column is required to send in body in case of
+  # create ServiceFabricPayment
+  # TODO: Make it generic
   def attributes_for_saving
     @unsaved_attributes = pointerize(@unsaved_attributes)
     put_attrs = @unsaved_attributes
