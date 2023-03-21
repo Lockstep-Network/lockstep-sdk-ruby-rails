16d320207f9e62a6b6042a452f4e1b35369f19cd
Ashish <ashish@lockstep.io>
2023-03-21 09:45:50 +0530

    fixed 405 issue

--- app/models/lockstep/payment_applied.rb
+++ app/models/lockstep/payment_applied.rb
@@ -3,4 +3,14 @@ class Lockstep::PaymentApplied < Lockstep::ApiRecord
   self.id_ref = "payment_applied_id"
   self.query_path = ""
   load_schema(Schema::PaymentApplied)
+
+  def self.merge_all_attributes(objects, response)
+    objects.each_with_index do |item, index|
+      next unless response[index]
+
+      new_attributes = response[index].transform_keys { |key| key.underscore }
+    end
+
+    true
+  end
 end
