dfeeff716611d3729343fc57b358e4af07bb50aa
Ashish <ashish@lockstep.io>
2023-03-22 00:01:06 +0530

    persisted removed from ServiceFabricPayment model

--- app/models/lockstep/service_fabric_payment.rb
+++ app/models/lockstep/service_fabric_payment.rb
@@ -1,6 +1,6 @@
 class Lockstep::ServiceFabricPayment < Lockstep::ApiRecord
   self.model_name_uri = "v1/payments-acceptance/payments/"
-  self.id_ref = "end_to_end_identifier"
+  self.id_ref = ""
   self.query_path=""
   load_schema(Schema::SfPaymentRequest)
   
@@ -8,9 +8,9 @@ class Lockstep::ServiceFabricPayment < Lockstep::ApiRecord
   # request body it returns true but in this model id_ref
   # is required field in request body
   # TODO: Make it generic 
-  def persisted?
-    false
-  end
+  # def persisted?
+  #   false
+  # end
 
   # Preventing deletion of id_ref from puts_attrs because
   # id_ref column is required to send in body in case of
@@ -25,9 +25,10 @@ class Lockstep::ServiceFabricPayment < Lockstep::ApiRecord
     put_attrs.delete('modified')
     put_attrs
   end
-  
+
   # Overriding This method to not send body inside an Array
   def create
+    debugger
     attrs = attributes_for_saving.transform_keys { |key| key.camelize(:lower) }
     resp = resource.post('', body: attrs)
     result = post_result(resp)
