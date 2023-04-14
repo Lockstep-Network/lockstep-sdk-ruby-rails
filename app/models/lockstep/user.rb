2a199c1570baa68bf6e8f98d7cf272eea44e054c
Lockstep APAC <lockstepapac@INXRFG6YCJ6Y.local>
2023-04-14 11:49:30 +0530

    Minor change

--- app/models/lockstep/user.rb
+++ app/models/lockstep/user.rb
@@ -3,6 +3,14 @@ class Lockstep::User < Lockstep::ApiRecord
   self.id_ref = "user_id"
   load_schema(Schema::UserAccount)
 
+  def self.invite(email_addresses)
+    emails = email_addresses.map { |email| { email: email } }
+    resp = resource.post('/invite', body: emails, params: {})
+    raise Lockstep::Exceptions::BadRequestError, 'Endpoint not found' if resp.code == '404'
+
+    result = JSON.parse(resp.body)
+  end
+
   # # Load the first account
   belongs_to :account, class_name: "Lockstep::Account", loader: ->(user) { Lockstep::Account.first }
 
