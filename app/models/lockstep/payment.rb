fa06315442629219866479e5d367447cc2789ac3
Ashish <ashish@lockstep.io>
2023-06-07 12:17:29 +0530

    Payment erp-write support added

--- app/models/lockstep/payment.rb
+++ app/models/lockstep/payment.rb
@@ -4,4 +4,17 @@ class Lockstep::Payment < Lockstep::ApiRecord
   load_schema(Schema::Payment)
 
   belongs_to :connection, class_name: "Lockstep::Connection", foreign_key: :company_id
+
+  # Overriding create to support erp-write in path params
+  def create
+    if attributes_for_saving.has_key?(:erp_write)
+      attributes_for_saving.delete(:erp_write)
+      path_params = 'erp-write'
+    else
+      path_params = ''
+    end
+    attrs = attributes_for_saving.transform_keys { |key| key.camelize(:lower) }
+    resp = resource.post(path_params, body: [attrs])
+    result = post_result(resp)
+  end
 end
