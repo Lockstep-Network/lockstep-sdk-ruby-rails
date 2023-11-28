b65bfc577948c85dfaaa566b841eca3cc90afab2
Tejas Shetty <74608924+shettytejas@users.noreply.github.com>
2023-11-28 22:04:04 +0530

    Attachment: Added download_url method
    * Adding method to fetch downloadLink for attachments
    * Gemfile.lock updates

--- app/models/lockstep/attachment.rb
+++ app/models/lockstep/attachment.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 class Lockstep::Attachment < Lockstep::ApiRecord
-  self.model_name_uri = "v1/Attachments"
-  self.id_ref = "attachment_id"
+  self.model_name_uri = 'v1/Attachments'
+  self.id_ref = 'attachment_id'
   load_schema(Schema::Attachment)
+
+  def download_url
+    response = resource.get "#{id}/download-url"
+    JSON.parse(response.body)['downloadLink']
+  end
 end
